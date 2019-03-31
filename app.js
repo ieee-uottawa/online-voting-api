181d5b3213c873ec4798c0df37c8569cf2d8d870
Rushil Perera <pererarushil1940@gmail.com>
2019-03-31 03:12:25 -0400

    Added endpoint to check if users can vote

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
+require('dotenv').load();
 const express = require('express');
 const cors = require('cors');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const dayjs = require('dayjs');
 
 const domains = ['http://localhost:3000', 'https://vote.ieeeuottawa.ca'];
 const app = express();
@@ -19,6 +21,14 @@ app.use(express.json());
 app.use(cookieParser());
 
 app.get('/ping', (req, res) => res.send('pong'));
+app.get('/can-vote', (req, res) => {
+    if (dayjs().isAfter(dayjs(process.env.START_TIME)) && dayjs().isBefore(dayjs(process.env.END_TIME))) {
+        res.status(200).send(null);
+    } else {
+        res.status(412).send(null);
+    }
+});
+
 app.use('/users', require('./routes/users_router'));
 app.use('/vote', require('./routes/vote_router'));
 
