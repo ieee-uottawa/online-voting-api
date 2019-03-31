09b94b0ebad0a406f5a30fcff1413148109d1bb3
Rushil Perera <pererarushil1940@gmail.com>
2019-03-31 02:35:06 -0400

    Changed platform migrations

--- migrations/v3-modify-platform-columns.sql
+++
@@ -1,8 +0,0 @@
-ALTER TABLE candidates
-  RENAME COLUMN platform TO platform_en;
-
-ALTER TABLE candidates
-  ALTER COLUMN platform_en DROP NOT NULL;
-
-ALTER TABLE candidates
-  ADD platform_fr TEXT;
---
+++ migrations/v3-remove-platform-columns.sql
@@ -0,0 +1,1 @@
+ALTER TABLE candidates DROP COLUMN platform;
