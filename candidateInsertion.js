d63e18f81b70077240b9ac7d547e9cade968f4a3
Rushil Perera <pererarushil1940@gmail.com>
2020-03-07 16:11:24 -0500

    Fixed issues where candidate and voter data was mixing

--- candidateInsertion.js
+++ candidateInsertion.js
@@ -14,25 +14,24 @@ async function main() {
     console.log('Inserting candidates');
 
     let filePath = 'data/votingCandidates.json';
-    let data = JSON.parse((await readFile(filePath)));
+    const candidateData = JSON.parse((await readFile(filePath)));
 
     const insertPositionQuery = 'INSERT INTO positions (name) VALUES ($1) RETURNING id;'
     const insertCandidateQuery = 'INSERT INTO candidates (name, position_id) VALUES ($1, $2);';
     const updatePositionNameQuery = 'UPDATE positions SET name = $1 WHERE id = $2;';
-    db.transaction(IsolationLevel.ReadUncommitted, (client) => {
-        Object.keys(data).forEach(async (positionName) => {
-            let position = positionName;
+    db.transaction(IsolationLevel.ReadUncommitted, async (client) => {
+        for (let position of Object.keys(candidateData)) {
             console.log(`Inserting "${position}"`);
             const { rows: [{ id: positionId }] } = await client.query(insertPositionQuery, [position]);
-            if (data[position].length > 1) {
-                data[position] = [
-                    ...data[position],
+            if (candidateData[position].length > 1) {
+                candidateData[position] = [
+                    ...candidateData[position],
                     { name: 'Abstain' },
                     { name: 'No Confidence' },
                 ];
-            } else if (data[position].length === 1) {
-                position = `${position} - ${data[position][0].name}`;
-                data[position] = [
+            } else if (candidateData[position].length === 1) {
+                position = `${position} - ${candidateData[position][0].name}`;
+                candidateData[position] = [
                     { name: 'Yes' },
                     { name: 'No' },
                     { name: 'Abstain' },
@@ -42,9 +41,9 @@ async function main() {
                 console.log(`Ignoring ${position}, no candidates`);
             }
             await Promise.all(
-                data[position].map(({ name }) => client.query(insertCandidateQuery, [name, positionId]))
+                candidateData[position].map(({ name }) => client.query(insertCandidateQuery, [name, positionId]))
             );
-        });
+        }
     });
 
     if (process.env.VALIDATE_USERS === 'true') {
@@ -52,12 +51,12 @@ async function main() {
         await db.query('DELETE FROM valid_users;');
 
         filePath = 'data/voters.json';
-        data = JSON.parse((await readFile(filePath)));
+        const voterData = JSON.parse((await readFile(filePath)));
 
         const insertUserQuery = 'INSERT INTO valid_users (email) VALUES ($1);';
         db.transaction(IsolationLevel.ReadUncommitted, async (client) => {
             console.log('Inserting valid voters');
-            await Promise.all(data.map(email => client.query(insertUserQuery, [email])));
+            await Promise.all(voterData.map(email => client.query(insertUserQuery, [email])));
         });
     }
 }
