7dae5ba53545a0e97f884025ff920807546ed912
Rushil Perera <pererarushil1940@gmail.com>
2020-03-25 13:20:05 -0400

    Created script to generate voter list from Excel sheet

---
+++ voterListGeneration.js
@@ -0,0 +1,19 @@
+require('dotenv').load();
+const XLSX = require('xlsx');
+const fs = require('fs');
+const { promisify } = require('util');
+
+const writeFile = promisify(fs.writeFile);
+
+const workbook = XLSX.readFile(process.env.VOTER_LIST);
+const sheet = workbook.Sheets[workbook.SheetNames[0]];
+
+writeFile(
+  'data/voters.json',
+  JSON.stringify(
+    XLSX.utils.sheet_to_json(sheet)
+      .map(({ email }) => email.toLowerCase())
+  )
+)
+  .catch(console.error);
+
